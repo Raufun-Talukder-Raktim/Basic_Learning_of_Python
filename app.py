07b02d1c872e7bb246944edd4b230df49477a672
Incognito_151 <74871689+Raufun-Talukder-Raktim@users.noreply.github.com>
2025-01-17 00:50:56 +0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+print("My name")
+print(8+3)
+print("*" * 10)
+price = 10
+price = 20
+rating = 4.5
+is_true = True
+print(price)
+
+patient_name = 'John Smith'
+patient_age = 20
+patient_status = 'new patient'
+is_new = True
+
+print(patient_name,patient_age,patient_status)
+
+name = input('what is you name? ')
+color = input('what is your favourite color? ')
+print(name +' likes ' + color)
