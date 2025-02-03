5147cde43318c6e1ba65954599e7a7ed381f9ff8
Incognito_151 <74871689+Raufun-Talukder-Raktim@users.noreply.github.com>
2025-02-03 21:16:48 +0600

    Commit by Raktim

--- Python Full Course for Beginners/modules.py
+++ Python Full Course for Beginners/modules.py
@@ -1,8 +1,6 @@
-def lbs_to_kg (weight):
-    return weight * 0.45
+import converters
+from converters import kg_to_lbs
 
+kg_to_lbs(100)
 
-def kg_to_lbs(weight):
-    return weight / 0.45
-
-
+print((converters.kg_to_lbs(70)))
