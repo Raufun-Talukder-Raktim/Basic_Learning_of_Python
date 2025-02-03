9f6d6b3dccbeb6058a12e76b0f38eefbb2a50831
Incognito_151 <74871689+Raufun-Talukder-Raktim@users.noreply.github.com>
2025-02-03 21:33:44 +0600

    Commit by Raktim

--- Python Full Course for Beginners/modules.py
+++ Python Full Course for Beginners/modules.py
@@ -1,6 +1,11 @@
 import converters
 from converters import kg_to_lbs
+from utils import find_max
 
+numbers = [10,3,6,2]
+maximum = find_max(numbers)
+
+print(maximum(numbers))
 kg_to_lbs(100)
 
 print((converters.kg_to_lbs(70)))
